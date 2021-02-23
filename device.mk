e2c7f24b0b68664b429eb99581afb40ecf610f52
Amy <00p513@protonmail.com>
2021-02-23 21:26:39 +0000

    channel: Initial changes for StyxOS

--- device.mk
+++ device.mk
@@ -47,7 +47,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tas2560.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tas2560.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Bluetooth
+PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
+
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SCREEN_HEIGHT := 1512
 TARGET_SCREEN_WIDTH := 720
 
